8d432563aec0c9684e1c7c48474da1a94939cbd8
Daan <daanl@outlook.com>
2021-07-15 11:35:26 -0700

    more comments

--- include/repline.h
+++ include/repline.h
@@ -23,7 +23,7 @@ extern "C" {
 struct rp_env_s;
 typedef struct rp_env_s rp_env_t;
 
-// Initialize and get the repline environment.
+// Initialize and create a repline environment.
 // See also: `rp_init_custom_alloc`.
 rp_env_t* rp_init(void);
 
@@ -31,23 +31,29 @@ rp_env_t* rp_init(void);
 // This is called automatically on program exit for unreleased environments.
 void rp_done(rp_env_t* env);
 
-// Read input for the user using rich editing abilities.
-// The `prompt_text` can be NULL for the default (""). The displayed prompt
-// becomes `prompt_text` followed by the `prompt_marker` ("> "). 
+// Read input from the user using rich editing abilities.
+// The `prompt_text` can be NULL for the default (""). 
+// The displayed prompt becomes `prompt_text` followed by the `prompt_marker` ("> "). 
+// Returns the heap allocated input on succes, which should be `free`d by the caller.  
 // Returns NULL on error, or if the user typed ctrl+d or ctrl+c.
-// Otherwise returns the heap allocated input which should be `free`d by the caller.  
+//
+// If the standard input (`stdin`) has no editing capability 
+// (like a dumb terminal (e.g. `TERM`=`dumb`), running in a debuggen, a pipe or redirected file, etc.)
+// the input is read directly from the input stream up to the 
+// next line without editing capability.
+//
 // See also: `rp_set_prompt_marker`, `rp_set_prompt_color`.
 char* rp_readline(rp_env_t* env, const char* prompt_text);   
 
 
 //--------------------------------------------------------------
-// history
+// History
 //--------------------------------------------------------------
 
 // Enable history. Use a NULL filename to not persist the history. Use -1 for max_entries to get the default (200).
 void rp_set_history(rp_env_t* env, const char* fname, long max_entries );
 
-// The last input is automatically added to the history; this function removes it.
+// The last returned input from `rp_readline` is automatically added to the history; this function removes it.
 void rp_history_remove_last(rp_env_t* env);
 
 // Clear the history.
@@ -64,12 +70,15 @@ void rp_history_clear(rp_env_t* env);
 typedef void (rp_completion_fun_t)(rp_env_t* env, const char* input, long cursor, void* arg );
 
 // Set the completion handler.
+// The `arg` is passed to every `completer` call by repline as is (and can be NULL).
+// This can be used to propagate user state to the `completer`.
 void rp_set_completer( rp_env_t* env, rp_completion_fun_t* completer, void* arg);
 
-// In a completion callback, use this to add completions.
+// In a completion callback, use this function to add completions.
 // When completed, `delete_before` _bytes_ are deleted before the cursor position,
 // `delete_after` _bytes_ are deleted after the cursor, and finally `completion` is inserted.
 // The `display` is used to display the completion in the completion menu.
+// (both `display` and `completion` are copied by repline and do not need to be preserved or allocated).
 //
 // Returns `true` if the callback should continue trying to find more possible completions.
 // If `false` is returned, the callback should try to return and not add more completions (for improved latency).
@@ -107,7 +116,7 @@ void rp_set_prompt_marker( rp_env_t* env, const char* prompt_marker );
 // Set the color used for the prompt text and marker.
 void rp_set_prompt_color( rp_env_t* env, rp_color_t color );
 
-// Enable/disable multi-line input.
+// Disable or enable multi-line input (enabled by default).
 void rp_set_multiline_input( rp_env_t* env, bool enable );
 
 
